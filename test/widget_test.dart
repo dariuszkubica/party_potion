122454fca9413d99fca06d1e85136166bcaec1ac
dariuszkubica <10threex01@gmail.com>
2022-12-13 22:11:45 +0100

    Add app page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:party_potion/main.dart';
+import 'package:party_potion/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
