8070fd5d921800ce4c7a108bb52c7481b7ea4932
dariuszkubica <10threex01@gmail.com>
2022-12-13 22:24:26 +0100

    Implement DevicePreview to app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,6 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      useInheritedMediaQuery: true,
+      builder: DevicePreview.appBuilder,
       home: Scaffold(),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:party_potion/app/app.dart';
 import 'package:party_potion/firebase_options.dart';
+import 'package:flutter/foundation.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
