396f3834fa043ba4d65eb05969cccd3fffc3eabe
dariuszkubica <10threex01@gmail.com>
2022-12-14 08:37:11 +0100

    Add BackgroundImageWIdget

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:party_potion/models/background_image_widget.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,7 +10,20 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       useInheritedMediaQuery: true,
       builder: DevicePreview.appBuilder,
-      home: Scaffold(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundImageWidget(
+      child: Column(),
     );
   }
 }
