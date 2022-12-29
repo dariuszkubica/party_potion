cb3798defea1acd513b673785e0f599fb91739ad
dariuszkubica <10threex01@gmail.com>
2022-12-29 22:07:47 +0100

    Implement ImageURL to CocktailModel

--- lib/models/cocktail_model.dart
+++ lib/models/cocktail_model.dart
@@ -2,12 +2,15 @@ class CocktailModel {
   const CocktailModel({
     required this.name,
     required this.instructions,
+    required this.imageURL,
   });
 
   final String name;
   final String instructions;
+  final String imageURL;
 
   CocktailModel.fromJson(Map<String, dynamic> json)
       : name = json['drinks'][0]['strDrink'],
-        instructions = json['drinks'][0]['strInstructions'];
+        instructions = json['drinks'][0]['strInstructions'],
+        imageURL = json['drinks'][0]['strDrinkThumb'];
 }
