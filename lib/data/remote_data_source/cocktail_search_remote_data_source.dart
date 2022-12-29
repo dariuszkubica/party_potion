3cd81e8d4ddfe1712fdf25e0d27a52472f6d8fe6
dariuszkubica <10threex01@gmail.com>
2022-12-29 20:39:53 +0100

    Add Dio handling errors

--- lib/data/remote_data_source/cocktail_search_remote_data_source.dart
+++ lib/data/remote_data_source/cocktail_search_remote_data_source.dart
@@ -4,8 +4,12 @@ class CocktailSearchRemoteDataSource {
   Future<Map<String, dynamic>?> getCocktailData({
     required String cocktailName,
   }) async {
-    final reponse = await Dio().get<Map<String, dynamic>>(
-        'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=$cocktailName');
-    return reponse.data;
+    try {
+      final reponse = await Dio().get<Map<String, dynamic>>(
+          'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=$cocktailName');
+      return reponse.data;
+    } on DioError catch (error) {
+      throw Exception(error.response?.data ?? 'Unknow error');
+    }
   }
 }
