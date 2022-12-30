6068d379bc890aeaf826c39e023e8b57e28305b5
dariuszkubica <10threex01@gmail.com>
2022-12-30 23:44:14 +0100

    Set same design for CocktailWidowDetail and SearchCocktailPage

--- lib/features/cocktail/details/cocktail_window_details.dart
+++ lib/features/cocktail/details/cocktail_window_details.dart
@@ -8,100 +8,90 @@ class CocktailWindowDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF202020),
-      body: BackgroundImageWidget(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(8, 56, 8, 62),
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black26,
-                    border: Border.all(
-                      width: 2,
-                      color: Colors.black,
-                    ),
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(50),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.white.withOpacity(0.2),
-                        spreadRadius: 0,
-                        blurRadius: 50,
-                        offset: const Offset(0, 0),
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: AspectRatio(
-                            aspectRatio: 1,
-                            child: CircleAvatar(
-                              backgroundImage:
-                                  const AssetImage('images/me.jpg'),
-                              backgroundColor: Colors.black.withOpacity(0.3),
-                            ),
-                          ),
-                        ),
-                        const Text(
-                          'Drink Name',
-                          style: TextStyle(
-                              color: Colors.red,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Expanded(
-                          child: ListView(
-                            padding: const EdgeInsets.all(8.0),
-                            children: [
-                              Column(
-                                children: const [
-                                  Text(
-                                    'Ingrediens:',
-                                    style: TextStyle(
-                                      color: Colors.red,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  SizedBox(height: 10),
-                                  Text('Ingredient 1',
-                                      style: TextStyle(color: Colors.white)),
-                                  Text('Ingredient 2',
-                                      style: TextStyle(color: Colors.white)),
-                                  Text('Ingredient 3',
-                                      style: TextStyle(color: Colors.white)),
-                                  Text('Ingredient 4',
-                                      style: TextStyle(color: Colors.white)),
-                                  Text('Ingredient 5',
-                                      style: TextStyle(color: Colors.white)),
-                                  SizedBox(height: 10),
-                                  Text(
-                                    'Instruction:',
-                                    style: TextStyle(
-                                        color: Colors.red, fontSize: 16),
-                                  ),
-                                  SizedBox(height: 10),
-                                  Text(
-                                    'Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+    return BackgroundImageWidget(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 32, 8, 32),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black54,
+            border: Border.all(
+              width: 2,
+              color: Colors.black,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(50),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white.withOpacity(0.2),
+                spreadRadius: 0,
+                blurRadius: 50,
+                offset: const Offset(0, 0),
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: CircleAvatar(
+                      backgroundImage: const AssetImage('images/me.jpg'),
+                      backgroundColor: Colors.black.withOpacity(0.3),
                     ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 15),
+                const Text(
+                  'Drink Name',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                Expanded(
+                  child: ListView(
+                    padding: const EdgeInsets.all(8.0),
+                    children: [
+                      Column(
+                        children: const [
+                          Text(
+                            'Ingrediens:',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text('Ingredient 1',
+                              style: TextStyle(color: Colors.white)),
+                          Text('Ingredient 2',
+                              style: TextStyle(color: Colors.white)),
+                          Text('Ingredient 3',
+                              style: TextStyle(color: Colors.white)),
+                          Text('Ingredient 4',
+                              style: TextStyle(color: Colors.white)),
+                          Text('Ingredient 5',
+                              style: TextStyle(color: Colors.white)),
+                          SizedBox(height: 10),
+                          Text(
+                            'Instruction:',
+                            style: TextStyle(color: Colors.red, fontSize: 16),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction Drink Instruction',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
