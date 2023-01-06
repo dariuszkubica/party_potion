67286c0dbdd350d79bc2d2b664e749790544bcbc
dariuszkubica <10threex01@gmail.com>
2023-01-06 15:42:49 +0100

    Change CocktailCubit and CocktailWindowsSmall for CocktailIngredientRepository

--- lib/common_widgets/cocktail_windows_small.dart
+++ lib/common_widgets/cocktail_windows_small.dart
@@ -1,77 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+//import 'package:party_potion/common_widgets/ingredient_widget.dart';
+import 'package:party_potion/features/cocktail/cubit/cocktail_cubit.dart';
 import 'package:party_potion/features/cocktail/details/cocktail_window_details.dart';
+import 'package:party_potion/models/cocktail_model.dart';
 
 class CocktailWindowSmall extends StatelessWidget {
   const CocktailWindowSmall({
     Key? key,
+    required this.cocktailModel,
   }) : super(key: key);
+
+  final CocktailModel cocktailModel;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context).push(
-            MaterialPageRoute(builder: (_) => const CocktailWindowDetails()));
-      },
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 5),
-        decoration: BoxDecoration(
-          color: Colors.black87,
-          image: const DecorationImage(
-            image: AssetImage('images/me.jpg'),
-            fit: BoxFit.cover,
-            opacity: 0.3,
-          ),
-          border: Border.all(
-            width: 2,
-            color: Colors.black,
-          ),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(50),
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                const Text(
-                  'Drink name',
-                  style: TextStyle(color: Colors.white),
-                ),
-                Column(
+    return BlocBuilder<CocktailCubit, CocktailState>(
+      builder: (context, state) {
+        return InkWell(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const CocktailWindowDetails()));
+          },
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 5),
+            decoration: BoxDecoration(
+              color: Colors.black87,
+              image: DecorationImage(
+                image: NetworkImage(cocktailModel.imageURL),
+                fit: BoxFit.cover,
+                opacity: 0.3,
+              ),
+              border: Border.all(
+                width: 2,
+                color: Colors.black,
+              ),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(50),
+              ),
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    const Text(
-                      'Ingredients:',
-                      style: TextStyle(color: Colors.white),
+                    Text(
+                      cocktailModel.name,
+                      style: const TextStyle(color: Colors.white),
                     ),
-                    const SizedBox(height: 5),
-                    SizedBox(
-                      width: 100,
-                      child: Column(
-                        children: const [
-                          Text('Ingredient 1',
-                              style: TextStyle(color: Colors.white)),
-                          Text('Ingredient 2',
-                              style: TextStyle(color: Colors.white)),
-                          Text('Ingredient 3',
-                              style: TextStyle(color: Colors.white)),
-                          Text('Ingredient 4',
-                              style: TextStyle(color: Colors.white)),
-                          Text('Ingredient 5',
-                              style: TextStyle(color: Colors.white)),
-                        ],
-                      ),
+                    Column(
+                      children: const [
+                        Text(
+                          'Ingredients:',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        SizedBox(height: 5),
+                        /* IngredientWidget(
+                            ingredientsList: cocktailModel.ingredientsList), */
+                      ],
                     )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/features/cocktail/cubit/cocktail_cubit.dart
+++ lib/features/cocktail/cubit/cocktail_cubit.dart
@@ -1,22 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:party_potion/app/core/enums.dart';
 import 'package:party_potion/models/cocktail_model.dart';
-import 'package:party_potion/repositories/cocktail_search_repository.dart';
+import 'package:party_potion/repositories/cocktail_ingredient_repository.dart';
 
 part 'cocktail_state.dart';
 
 class CocktailCubit extends Cubit<CocktailState> {
-  CocktailCubit(this._cocktailSearchRepository) : super(const CocktailState());
+  CocktailCubit(this._cocktailIngredientRepository)
+      : super(const CocktailState());
 
-  final CocktailSearchRepository _cocktailSearchRepository;
+  final CocktailIngredientRepository _cocktailIngredientRepository;
 
-  Future<void> getCocktailModelByName({
-    required String cocktailName,
+  Future<void> getCocktailModelByAlcohol({
+    required String alcoholName,
   }) async {
     emit(const CocktailState(status: Status.loading));
     try {
-      final cocktailModel = await _cocktailSearchRepository
-          .getCocktailModelByName(cocktailName: cocktailName);
+      final cocktailModel = await _cocktailIngredientRepository
+          .getCocktailModelByAlcohol(alcoholName: alcoholName);
       emit(
         CocktailState(
           model: cocktailModel,
