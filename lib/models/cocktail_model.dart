41670eed6e3fece3f32dfffbc52bfc5b50e9b4cd
dariuszkubica <10threex01@gmail.com>
2023-02-22 17:13:37 +0100

    Add ignore comments

--- lib/models/cocktail_model.dart
+++ lib/models/cocktail_model.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_annotation_target
+
 import 'package:party_potion/app/core/ingredients_converter.dart';
 import 'package:party_potion/models/ingredient_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
