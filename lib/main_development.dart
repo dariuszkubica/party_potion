5db6fa26d89e9397d6edd0384d4b54461b257c9e
dariuszkubica <10threex01@gmail.com>
2023-03-31 22:16:50 +0200

    Fix Implementation GetIt()

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,11 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:party_potion/app/app.dart';
 import 'package:party_potion/app/core/config.dart';
+import 'package:party_potion/app/injection_container.dart';
 import 'package:party_potion/firebase_options.dart';
 import 'package:flutter/foundation.dart';
 import 'package:device_preview/device_preview.dart';
 
 void main() async {
+  configureDependencies();
   Config.appFlavor = Flavor.development;
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:party_potion/app/app.dart';
 import 'package:party_potion/app/core/config.dart';
+import 'package:party_potion/app/injection_container.dart';
 import 'package:party_potion/firebase_options.dart';
 
 void main() async {
+  configureDependencies();
   Config.appFlavor = Flavor.staging;
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
