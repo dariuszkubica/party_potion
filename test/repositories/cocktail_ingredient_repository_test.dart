fd0ff4d6461b9637435c4e674c7ed7077480aa6d
dariuszkubica <10threex01@gmail.com>
2023-04-04 18:55:17 +0200

    Add cocktail_ingredient_repository_test.dart

---
+++ test/repositories/cocktail_ingredient_repository_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:party_potion/data/remote_data_source/cocktail_ingredient_remote_retrofit_data_source.dart';
+import 'package:party_potion/models/drinks_dto.dart';
+import 'package:party_potion/repositories/cocktail_ingredient_repository.dart';
+
+class MockCocktailIngredientDataSource extends Mock
+    implements CocktailIngredientRemoteRetroFitDataSource {}
+
+void main() {
+  late CocktailIngredientRepository sut;
+  late MockCocktailIngredientDataSource dataSource;
+
+  setUp(() {
+    dataSource = MockCocktailIngredientDataSource();
+    sut = CocktailIngredientRepository(
+        cocktailIngredientRemoteRetroFitDataSource: dataSource);
+  });
+
+  group('getCocktailModelsByAlcohol', () {
+    test(
+        'should call remoteDataSource.getCocktailDataByAlcohol(alcoholName) method',
+        () async {
+      // 1
+      when(() => dataSource.getCocktailDataByAlcohol('vodka'))
+          .thenAnswer((_) async => const DrinksDTO(drinks: []));
+      // 2
+      await sut.getCocktailModelsByAlcohol(alcoholName: 'vodka');
+      // 3
+      verify(() => dataSource.getCocktailDataByAlcohol('vodka')).called(1);
+    });
+
+    test('should filter data source results with provided alcohol name',
+        () async {
+      // 1
+      when(() => dataSource.getCocktailDataByAlcohol('vodka'))
+          .thenAnswer((_) async => const DrinksDTO(drinks: []));
+      // 2
+      final results =
+          await sut.getCocktailModelsByAlcohol(alcoholName: 'vodka');
+      // 3
+      expect(results, const DrinksDTO(drinks: []));
+    });
+  });
+}
