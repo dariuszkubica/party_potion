95368766cb46856d38343e8e5686c0d37596f2a9
dariuszkubica <10threex01@gmail.com>
2023-04-12 17:41:46 +0200

    Add missing IngredientsList to CocktailSearchRepositoryTest

--- test/repositories/cocktail_search_repository_test.dart
+++ test/repositories/cocktail_search_repository_test.dart
@@ -3,6 +3,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:party_potion/data/remote_data_source/cocktail_search_remote_retrofit_data_source.dart';
 import 'package:party_potion/models/cocktail_dto.dart';
 import 'package:party_potion/models/cocktail_model.dart';
+import 'package:party_potion/models/ingredient_model.dart';
 import 'package:party_potion/repositories/cocktail_search_repository.dart';
 
 class MockCocktailSearchDataSource extends Mock
@@ -26,9 +27,40 @@ void main() {
       when(() => dataSource.getCocktailDataByName('mojito')).thenAnswer(
         (_) async => const CocktailsDto(drinks: [
           CocktailDto(
-              strDrink: 'mojito',
-              strInstructions: '',
-              strDrinkThumb: 'imageURL')
+            strDrink: 'mojito',
+            strInstructions: '',
+            strDrinkThumb: 'imageURL',
+            strIngredient1: '',
+            strIngredient2: '',
+            strIngredient3: '',
+            strIngredient4: '',
+            strIngredient5: '',
+            strIngredient6: '',
+            strIngredient7: '',
+            strIngredient8: '',
+            strIngredient9: '',
+            strIngredient10: '',
+            strIngredient11: '',
+            strIngredient12: '',
+            strIngredient13: '',
+            strIngredient14: '',
+            strIngredient15: '',
+            strMeasure1: '',
+            strMeasure2: '',
+            strMeasure3: '',
+            strMeasure4: '',
+            strMeasure5: '',
+            strMeasure6: '',
+            strMeasure7: '',
+            strMeasure8: '',
+            strMeasure9: '',
+            strMeasure10: '',
+            strMeasure11: '',
+            strMeasure12: '',
+            strMeasure13: '',
+            strMeasure14: '',
+            strMeasure15: '',
+          )
         ]),
       );
       // 2
@@ -43,9 +75,40 @@ void main() {
       when(() => dataSource.getCocktailDataByName('mojito')).thenAnswer(
         (_) async => const CocktailsDto(drinks: [
           CocktailDto(
-              strDrink: 'mojito',
-              strInstructions: '',
-              strDrinkThumb: 'imageURL')
+            strDrink: 'mojito',
+            strInstructions: '',
+            strDrinkThumb: 'imageURL',
+            strIngredient1: '',
+            strIngredient2: '',
+            strIngredient3: '',
+            strIngredient4: '',
+            strIngredient5: '',
+            strIngredient6: '',
+            strIngredient7: '',
+            strIngredient8: '',
+            strIngredient9: '',
+            strIngredient10: '',
+            strIngredient11: '',
+            strIngredient12: '',
+            strIngredient13: '',
+            strIngredient14: '',
+            strIngredient15: '',
+            strMeasure1: '',
+            strMeasure2: '',
+            strMeasure3: '',
+            strMeasure4: '',
+            strMeasure5: '',
+            strMeasure6: '',
+            strMeasure7: '',
+            strMeasure8: '',
+            strMeasure9: '',
+            strMeasure10: '',
+            strMeasure11: '',
+            strMeasure12: '',
+            strMeasure13: '',
+            strMeasure14: '',
+            strMeasure15: '',
+          )
         ]),
       );
       // 2
@@ -57,7 +120,23 @@ void main() {
             cocktailName: 'mojito',
             instructions: '',
             imageURL: 'imageURL',
-            ingredientsList: [],
+            ingredientsList: [
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+              IngredientModel(ingredientName: '', ingredientMesure: ''),
+            ],
           ));
     });
   });
