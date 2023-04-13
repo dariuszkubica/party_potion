810938383c226ef3ff3e75158b2f4de43d60695e
dariuszkubica <10threex01@gmail.com>
2023-04-13 09:08:58 +0200

    Add AlcoholModel

---
+++ lib/models/alcohol_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'alcohol_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_AlcoholModel _$$_AlcoholModelFromJson(Map<String, dynamic> json) =>
+    _$_AlcoholModel(
+      id: json['id'] as String,
+      alcoholName: json['alcoholName'] as String,
+      alcoholUrl: json['alcoholUrl'] as String?,
+      have: json['have'] as bool,
+    );
+
+Map<String, dynamic> _$$_AlcoholModelToJson(_$_AlcoholModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'alcoholName': instance.alcoholName,
+      'alcoholUrl': instance.alcoholUrl,
+      'have': instance.have,
+    };
